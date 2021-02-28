c23044083216bd7471fb6152cf4e2bd2f95d98b4
thebeardless <jakepeterwebster@gmail.com>
2021-02-28 22:11:49 +1000

    MVP

--- testFiles/syntaxHighlight.js
+++ testFiles/syntaxHighlight.js
@@ -17,3 +17,25 @@ function $initHighlight(block, cls) {
 }
 
 const testing = 4;
+
+import * as api from "../api";
+
+// Action Creators
+export const getPosts = () => async (dispatch) => {
+  try {
+    const { data } = await api.fetchPosts();
+    dispatch({ type: "FETCH_ALL", payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const createPost = (post) => async (dispatch) => {
+  try {
+    const { data } = await api.createPost(post);
+
+    dispatch({ type: "CREATE", payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
